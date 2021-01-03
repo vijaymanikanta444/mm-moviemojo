440b02e84c67690fe0c3139f115dd29cd5f14b2c
reshmakandregula <reshma.kandregula1999@gmail.com>
2021-01-03 18:14:09 +0530

    added topNavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
-import Test98 from './components/Test98';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import TopNavbar from "./components/layout/TopNavBar";
+import Test98 from "./components/Test98";
 
 function App() {
   return (
     <div className="App">
-      <h1>client Setup</h1>
-      <Test98 />
+      <Router>
+        <TopNavbar />
+        <h1>client Setup</h1>
+
+        <Test98 />
+      </Router>
     </div>
   );
 }
