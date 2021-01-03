8dbd67003f10c72248efa9f7f4e39f2c5e0b276f
reshmakandregula <reshma.kandregula1999@gmail.com>
2021-01-03 19:05:41 +0530

    added icons to TopNavBar

--- src/components/layout/TopNavBar.js
+++ src/components/layout/TopNavBar.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import Logo from "./Logo1.png";
 
-const TopNavbar = () => {
-  return (
-    <div className="Navbar">
-      <div className="leftSide">
-        <img src={Logo} height="65px" width="150px" alt="" />
+const TopNavbar = () => (
+  <div className="Navbar">
+    <div className="leftSide">
+      <img src={Logo} height="65px" width="190px" alt="" />
+    </div>
+    <div className="rightSide">
+      <div>
+        <input type="text" placeholder="Search Here..." />
+        <button type="submit">
+          <FaIcons.FaSearch />
+        </button>
       </div>
-      <div className="rightSide">
-        <div>
-          <input type="text" placeholder="Search Here..." />
-          <button>
-            <FaSearch />
-          </button>
-        </div>
 
-        <div className="links">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </div>
+      <div className="links">
+        <Link to="/login" className="logoStyle">
+          <AiIcons.AiOutlineLogin style={{ paddingRight: 5 }} />
+          Login
+        </Link>
+        <Link to="/register" className="logoStyle">
+          <FaIcons.FaRegRegistered style={{ paddingRight: 5 }} />
+          Register
+        </Link>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default TopNavbar;
