f77a8347a98e5708ded75b3ffaeaa5357edae858
vijaymanikanta444 <vijayakp444@gmail.com>
2021-01-04 01:18:07 +0530

    proxy setup added

--- src/components/Test98.js
+++ src/components/Test98.js
@@ -1,9 +1,17 @@
-import React from "react";
+import axios from 'axios';
+import React, { useEffect } from 'react';
 
-const Test98 = () => (
-  <div>
-    <h1>this is a client setup, test:MM-0005</h1>
-  </div>
-);
+const Test98 = () => {
+  // const [data, setData] = useState(0);
 
+  useEffect(() => {
+    axios.get('/movies').then((res) => console.log(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h1>this is a client setup, test:MM-0005</h1>
+    </div>
+  );
+};
 export default Test98;
